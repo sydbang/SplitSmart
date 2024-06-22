ffbb0cb322171c322c4e3fe2ac9846bf91bcb1de
Sydney Bang <sydney.bang94@gmail.com>
2024-06-22 12:38:49 -0700

    creating xcode file

---
+++ SplitSmart/SplitSmart/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SplitSmart
+//
+//  Created by Sunghee Bang on 2024-06-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SplitSmart/SplitSmart/SplitSmartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitSmartApp.swift
+//  SplitSmart
+//
+//  Created by Sunghee Bang on 2024-06-22.
+//
+
+import SwiftUI
+
+@main
+struct SplitSmartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
