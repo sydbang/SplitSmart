139a5df7fbdb81ec20e5851660c52e7026103757
Sydney Bang <sydney.bang94@gmail.com>
2024-06-22 20:28:55 -0700

    create Person model

---
+++ SplitSmart/SplitSmart/ViewModels/PersonModel.swift
@@ -0,0 +1,14 @@
+//
+//  PersonModel.swift
+//  SplitSmart
+//
+//  Created by Sunghee Bang on 2024-06-22.
+//
+
+import Foundation
+
+@Observable
+class PersonModel {
+    
+    //TODO: have list of people available here and publish
+}
